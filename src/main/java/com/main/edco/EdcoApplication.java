91882bc7e7610b4ea73e4a1e99cb15c76dddb875
abid <abid.rashid@live.com>
2021-01-30 19:23:04 +0900

    Base files

---
+++ src/main/java/com/main/edco/EdcoApplication.java
@@ -0,0 +1,13 @@
+package com.main.edco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdcoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdcoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/main/edco/EdcoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main.edco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdcoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
